b9f3ca3610c3fd3184f237f65871d42aa43adead
David Rodriguez <davidr170298@gmail.com>
2018-04-04 20:56:16 -0600

    Second Git commit

--- src/Graphs.java
+++ src/Graphs.java
@@ -6,22 +6,22 @@ import java.nio.file.Files;
 
 public class Graphs {
 	
-	Prim p;
 	Kruskal k;
 	FloydWarshall f;
 	
 	public static void main(String[] args) 
 	{
-		Read r=new Read();
+		Prim p=new Prim();
+		p.MST();
 	}
 }
 
 ////////////////////////////////////////////////////////////////
 
 class Read{
-	
+
 	public String[] labels= new String[5];
-	public Integer[][] adjacency= new Integer[5][5];
+	public int[][] adjacency= new int[5][5];
 	public Read() {
 		int count=0;
 		Charset charset=Charset.forName("US-ASCII");
@@ -84,12 +84,77 @@ class Read{
 				System.err.format("IOException: %s%n", x);
 			}	
 	}
+	public int getSize() {
+		return labels.length;
+	}
+	public String getName(int i) {
+		return labels[i];
+	}
+	
+	public int[][] getValue() {
+		return adjacency;
+	}
 }
 
 ///////////////////////////////////////////////////////////////
 
 class Prim{
 	
+	public Read r;
+	public void MST() 
+	{
+		r= new Read();
+		int value[][]=r.getValue();
+		int size=r.getSize();
+		int key[]=new int[size];
+		int name[]=new int[size];
+		Boolean set[]=new Boolean[size];
+		
+		for(int i=0; i<size; i++) {
+			key[i]=Integer.MAX_VALUE;
+			set[i]=false;
+		}
+		
+		key[0]=0;
+		name[0]=-1;
+		
+		for (int i=0; i<size-1;i++) {
+			int min=findMin(key,set);
+			
+			set[i]=true;
+			
+			for(int j=0; j<size;j++) {
+				if(set[j]==false && value[min][j]!=0 && value[min][j]<key[j]) {
+					name[j]=min;
+					key[j]=value[min][j];
+				}
+			}
+		}
+		print(name, value);
+	}
+	
+	public int findMin(int key[], Boolean set[]) 
+	{
+		int size=r.getSize();
+		int min=Integer.MAX_VALUE;
+		int min2=-1;
+		for(int i=0; i<size; i++) {
+			if(set[i]==false && key[i]<min) {
+				min=key[i];
+				min2=i;
+			}
+		}
+		return min2;
+	}
+	public void print(int[] name, int[][] value) {
+		int size=r.getSize();
+		String[] labels=new String[size];
+		
+		for (int i=1; i<size; i++) {
+			System.out.println(name[i]	+	"-"	+i);
+		}
+		
+	}
 }
 
 /////////////////////////////////////////////////////
